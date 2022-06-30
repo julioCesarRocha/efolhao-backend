802fec9e244989b2b0f76a0f172a8d98cfd7b5b0
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2022-06-29 21:59:01 -0300

    Corrigindo erro de CORS

--- backend/settings.py
+++ backend/settings.py
@@ -41,6 +41,8 @@ INSTALLED_APPS = [
     'sinaisvitais',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
