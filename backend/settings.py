564417932281d2f5869edf0f8f5f944220dc82b6
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2022-09-28 22:36:12 -0300

    Configuração com mongodb

--- backend/settings.py
+++ backend/settings.py
@@ -76,23 +76,21 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
-
+'''''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+'''''
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'djongo',
-#         'NAME': 'your-db-name',
-#         'CLIENT': {
-#            'host': 'your-db-host',
-#         }
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'djongo',
+        'NAME': 'efolhao_db',
+    }
+}
 
 
 # Password validation
