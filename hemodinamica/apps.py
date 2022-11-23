1d5514803454f11b6d64a9c02e76993b206cc041
Julio Cesar <julio.junior1@estudante.ifb.edu.br>
2022-11-22 23:00:17 -0300

    Finalizando insert módulo SV e iniciando módulo Hemodinamica

---
+++ hemodinamica/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class HemodinamicaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'hemodinamica'
--- sinaisvitais/urls.py
+++ sinaisvitais/urls.py
@@ -2,6 +2,6 @@ from django.urls import re_path
 from sinaisvitais import views
 
 urlpatterns = [
-    re_path(r'^sinaisvitais/$', views.sinaisVitaisApi),
-    re_path(r'^sinaisvitais/([0-9]+)$', views.sinaisVitaisApi),
+    re_path(r'^sinaisvitais', views.sinaisVitaisApi),
+    re_path(r'^sinaisvitais', views.sinaisVitaisApi),
 ]   
